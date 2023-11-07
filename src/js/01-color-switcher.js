216c0fb9c653a58390dcea48cb7144a4cd0e9a25
NatNovs <136241094+NatNovs@users.noreply.github.com>
2023-11-07 22:43:51 +0100

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -15,6 +15,8 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 } 
 
+
+
 function onBtnStartRainbow() {
     refs.btnStart.disabled = true;
     refs.btnStop.disabled = false;
